f1d06a09bf7f8854b30c202026030672dfde4765
miguelprian <99908488+miguelprian@users.noreply.github.com>
2026-04-10 15:32:30 +0200

    main.py

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -6,7 +6,7 @@ from routers import users
 from routers import basic_auth_users
 from routers import jwt_auth_users
 from routers import users_db
-from fastapi.staticfiles import StaticFiles
+#from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
