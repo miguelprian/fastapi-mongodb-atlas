6f789fc98553e8948466187dff0748149baa0d23
miguelprian <99908488+miguelprian@users.noreply.github.com>
2026-04-10 16:13:05 +0200

    main.py

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,21 +1,21 @@
 #uvicorn main:app --reload
 
 from fastapi import FastAPI
-#from routers import products
-#from routers import users
-#from routers import basic_auth_users
-#from routers import jwt_auth_users
+from routers import products
+from routers import users
+from routers import basic_auth_users
+from routers import jwt_auth_users
 from routers import users_db
-#from fastapi.staticfiles import StaticFiles
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-#app.include_router(products.router_products)
-#app.include_router(users.router_users)
-#app.include_router(basic_auth_users.router_bau)
-#app.include_router(jwt_auth_users.router_jau)
+app.include_router(products.router_products)
+app.include_router(users.router_users)
+app.include_router(basic_auth_users.router_bau)
+app.include_router(jwt_auth_users.router_jau)
 app.include_router(users_db.router_users_db)
-#app.mount("/statics", StaticFiles(directory="statics"), name="statics")
+app.mount("/statics", StaticFiles(directory="statics"), name="statics")
 
 @app.get("/")
 
